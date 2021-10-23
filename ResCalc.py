b58e9a0e53307d36f217d99e9b0b33ccdd55832e
AliAzarafza <aliazarafza2002@gmail.com>
2021-10-23 10:09:38 +0330

    finish

--- ResCalc.py
+++ ResCalc.py
@@ -4,20 +4,20 @@ import ResFunction
 resistor = '\033[0;31;40m resistor'
 
 print(f'''
-...welcom to {resistor} calculator...
-Operation number one parallel resistor
-Operation number two Series resistor
+...welcom to{resistor}\033[0;37;40m calculator...
+Operation number one parallel resistor *1*
+Operation number two Series resistor *2*
 ''')
 
 
-opration = input('number of the opration : ')
-Res1 = input('The first resistance : ')
-Res2 = input('The second resistance : ')
+opration = input('\033[0;34;40m number of the opration : ')
+Res1 = input('\033[0;36;40m The first resistance : ')
+Res2 = input('\033[0;36;40m The second resistance : ')
 
 
 if opration == '1':
-    ResFunction.ParallelRes(Res1, Res2)
+    ResFunction.ParallelRes(int(Res1), int(Res2))
 elif opration == '2':
-    ResFunction.SeriesRes(Res1, Res2)
+    ResFunction.SeriesRes(int(Res1), int(Res2))
 else:
-    print('\033[0;31;43m Error Please enter a valid opration : ')
+    print('\033[0;31;43m Error Please enter a valid opration (1 or 2)')
--- ResFunction.py
+++ ResFunction.py
@@ -1,10 +1,10 @@
 
 def ParallelRes(res1,res2):
-    resv = (res1 * res2) / (res1 + res2)
-    print(f'\033[0;36;42m The resistance value is equal to = {resv}')
+    resv = ((res1 * res2) / (res1 + res2))
+    print(f'\033[0;34;47m The resistance value is equal to = {resv} \033[0;34;40m')
 
 
 def SeriesRes(res1,res2):
-    resv = res1 + res2
-    print(f'\033[0;36;42m The resistance value is equal to = {resv}')
+    resv = (res1 + res2)
+    print(f'\033[0;34;47m The resistance value is equal to = {resv} \033[0;34;40m')
 
