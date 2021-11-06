2a2bb6729225aa342023e43165a27a1bb9c43eb3
AliAzarafza <aliazarafza2002@gmail.com>
2021-11-06 08:42:04 +0330

    triangle to star

--- ResCalc.py
+++ ResCalc.py
@@ -18,9 +18,10 @@ resistor = '\033[0;31;40m resistor\033[0;37;40m'
 
 print(f'''
     ...welcom to{resistor} calculator...
-    Operation number one parallel resistor *1*
-    Operation number two Series resistor *2*
-    Operation number tree for Star to triangle *3*
+    Operation number one parallel resistor [1]
+    Operation number two Series resistor [2]
+    Operation number tree for Star to triangle [3]
+    Operation number four for triangle to star [4]
 ''')
 
 
@@ -40,6 +41,11 @@ elif opration == '3':
     R2 = input('\033[0;36;40m The R2 value : \033[0;37;40m')
     R3 = input('\033[0;36;40m The R3 value : \033[0;37;40m')
     ResFunction.Star2Tra(float(R1),float(R2),float(R3))
+elif opration == '4':
+    RA = input('\033[0;36;40m The RA value : \033[0;37;40m')
+    RB = input('\033[0;36;40m The RB value : \033[0;37;40m')
+    RC = input('\033[0;36;40m The RC value : \033[0;37;40m')
+    ResFunction.Tar2Star(float(RA),float(RB),float(RC))
 else:
     print('\033[0;31;43m Error Please enter a valid opration (1 or 2) \033[0;37;40m')
 
--- ResFunction.py
+++ ResFunction.py
@@ -19,6 +19,13 @@ def SeriesRes(res1,res2):
 
 def Star2Tra(R1,R2,R3):
     p1 = (R1*R2+R1*R3+R2*R3)
-    RA = p1 / R1
-    RA = truncate(RA, 4)
-    print(f'\033[0;34;47m The \033[0;31;47mresistance\033[0;34;47m value is equal to = {RA} \033[0;34;40m')
+    RA = truncate((p1 / R1), 4)
+    RB = truncate((p1 / R2), 4)
+    RC = truncate((p1 / R3), 4)
+    print(f'\033[0;34;47mthe\033[0;31;47m RA \033[0;34;47mvalue is equal to = {RA}\033[0;34;40m\n\033[0;34;47mthe\033[0;31;47m RB \033[0;34;47mvalue is equal to = {RB}\033[0;34;40m\n\033[0;34;47mthe\033[0;31;47m RC \033[0;34;47mvalue is equal to = {RC}\033[0;34;40m')
+
+def Tar2Star(RA,RC,RB):
+    R1 = truncate(((RB*RC)/(RA+RB+RC)), 4)
+    R2 = truncate(((RA*RC)/(RA+RB+RC)), 4)
+    R3 = truncate(((RA*RB)/(RA+RB+RC)), 4)
+    print(f'\033[0;34;47mthe\033[0;31;47m R1 \033[0;34;47mvalue is equal to = {R1}\033[0;34;40m\n\033[0;34;47mthe\033[0;31;47m R2 \033[0;34;47mvalue is equal to = {R2}\033[0;34;40m\n\033[0;34;47mthe\033[0;31;47m R3 \033[0;34;47mvalue is equal to = {R3}\033[0;34;40m')
